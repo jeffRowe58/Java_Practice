01d63718619530b45d696445298c69d51d2e0684
Jeffrey Rowe <jeffrey.r.rowe58@gmail.com>
2021-06-12 16:22:23 -0500

    book work and ContactApp  sandbox

---
+++ src/Contacts/Contacts.java
@@ -0,0 +1,25 @@
+package Contacts;
+
+public class Contacts {
+    private String name;
+    private String number;
+
+    public Contacts(String name, String number) {
+        this.name=name;
+        this.number=number;
+    }
+
+    public Contacts() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+
+}
--- src/FileIO.java
+++ src/FileIO.java
@@ -1,9 +1,24 @@
 import java.io.IOException;
 import java.nio.file.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class FileIO {
+
+    public void readFileAndOutput(Path pathToFile) {
+        List<String> currentList = new ArrayList<>();
+        try {
+            currentList = Files.readAllLines(pathToFile);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+        for (String line : currentList) {
+            System.out.println(line);
+        }
+    }
 
     public static void main(String[] args) {
         Path path = Paths.get("src");
@@ -34,12 +49,65 @@ public class FileIO {
 //        }catch(IOException ioe){
 //            ioe.printStackTrace();
 //        }
-        List<String> romanEmpresses = Arrays.asList("Livia", "Agrippina", "Messaline", "Julia Domna");
+//        List<String> romanEmpresses = Arrays.asList("Livia", "Agrippina", "Messaline", "Julia Domna");
+//
+//        try{
+//            Files.write(toOurDataFile, romanEmpresses);
+//        }catch (IOException ioe){
+//            ioe.printStackTrace();
+//        }
 
+        //Read from a file
+
+        //create a place to put information from file
+//        List<String> currentList = new ArrayList<>();
+//
+//        try{
+//            currentList = Files.readAllLines(toOurDataFile);
+//        }catch(IOException ioe){
+//            ioe.printStackTrace();
+//        }
+//
+//        for(String line : currentList){
+//            System.out.println(line);
+//        }
+        FileIO io = new FileIO();
+//        io.readFileAndOutput(toOurDataFile);
+
+//        try{
+//            Files.writeString(toOurDataFile, "Julia Maesa\n", StandardOpenOption.APPEND);
+//        }catch (IOException ioe){
+//            ioe.printStackTrace();
+//        }
+//        List<String> lateRomanEmpresses = new ArrayList<>();
+//        lateRomanEmpresses.add("Galla Placidia");
+//        lateRomanEmpresses.add("Thodora");
+//        lateRomanEmpresses.add("Licinia Eudoxia");
+//
+//        try{
+//            Files.write(toOurDataFile, lateRomanEmpresses, StandardOpenOption.APPEND);
+//        }catch(IOException ioe){
+//            ioe.printStackTrace();
+//        }
+//
+        List<String> currentList = new ArrayList<>();
         try{
-            Files.write(toOurDataFile, romanEmpresses);
-        }catch (IOException ioe){
+            currentList = Files.readAllLines(toOurDataFile);
+        }catch(IOException ioe){
             ioe.printStackTrace();
         }
+        Iterator<String> listIterator = currentList.iterator();
+        while(listIterator.hasNext()) {
+            String empress = listIterator.next();
+            if(empress.equals("Galla Placidia")){
+                listIterator.remove();
+            }
+        }
+        try{
+            Files.write(toOurDataFile, currentList);
+        }catch(IOException ioe){
+            ioe.printStackTrace();
+        }
+        io.readFileAndOutput(toOurDataFile);
     }
 }
